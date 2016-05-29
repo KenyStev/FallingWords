cd91a887bde41203106e0b53f8db90c254bf2d76
kenystev <kjestevez.0795@gmail.com>
2016-05-28 23:45:02 -0600

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <GL/gl.h>
+#include <stdio.h>
+
+#include "Rosalila/system.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+//SDL
+#include "SDL2/SDL.h"
+#include "Rosalila/RosalilaUtility/RosalilaUtility.h"
+#include "Rosalila/RosalilaInputs/RosalilaInputs.h"
+#include "Rosalila/RosalilaGraphics/RosalilaGraphics.h"
+#include "Rosalila/RosalilaSound/RosalilaSound.h"
+
+#ifdef LINUX
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#endif
+
+#ifdef WINDOWS
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#endif
+
+#ifdef OSX
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2_mixer/SDL_mixer.h>
+#endif
+
+#include "Font.h"
+
+int main(int argc, char *argv[])
+{
+    //Clean the previous log
+    clearLog();
+
+    //Input receiver initialization
+    Receiver* receiver = new Receiver();
+
+    //Graphics initialization
+    RosalilaGraphics* rosalila_graphics = new RosalilaGraphics();
+    rosalila_graphics->video(rosalila_graphics);
+
+    float x=0;
+    float y=0;
+    float size=10;
+
+    Font *miPalabra = new Font("font.ttf");
+    miPalabra->setColor(255,255,255);
+    miPalabra->setSize(10);
+
+    while(true)
+    {
+        miPalabra->drawText("Hola",100,10);
+
+        receiver->updateInputs();
+        rosalila_graphics->updateScreen();
+    }
+
+
+    return 0;
+}
